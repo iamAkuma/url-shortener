f00e668d3b7760e4d95c8e4650360094bde021de
Saral Bajracharya <rmanbajra@gmail.com>
2023-06-04 20:12:13 +0545

    URL-shortener

--- config/db.js
+++ config/db.js
@@ -3,17 +3,17 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true
-        });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true
+    });
 
-        console.log('MongoDB connected...');
-    } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-        
-    }
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-//COnnect to database
+// Connect to database
 connectDB();
 
-app.use(express.json({ extended: false}));
+app.use(express.json());
 
-//Define Routes
+// Define Routes
 app.use('/', require('./routes/index'));
-app.use('/api/uri', require('./routes/url'));
+app.use('/api/url', require('./routes/url'));
 
 const PORT = 5000;
 
-app.listen(PORT, () => console.log( `Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
